dd5ecb69e9f82f4ca9c31a529687f4ec35db5587
Prof. Celso <celsofurtado@terra.com.br>
2021-12-21 14:57:13 -0300

    Versao 01

---
+++ app/src/main/java/br/pro/celsofurtado/fragment_avanade_prof/UsersFragment.kt
@@ -0,0 +1,25 @@
+package br.pro.celsofurtado.fragment_avanade_prof
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class UsersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false)
+    }
+
+}
